76e01831e61d4ef3b323dd5f1bf9400b2942c6ea
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-06-23 17:06:47 +0000

    add DB connection string

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/JE-Advertisement-Web-App");
+  //   await mongoose.connect("mongodb://localhost:27017/JE-Advertisement-Web-App");
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log("DB connected Successfully!");
 };
 
