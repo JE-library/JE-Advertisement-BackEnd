8ae856ec26d45ea88d48ca63af025b628c20e823
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-06-24 12:25:14 +0000

    feat: reconnected data base to Atlas

--- configs/db.config.js
+++ configs/db.config.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/JE-Advertisement-Web-App");
-  // await mongoose.connect(process.env.MONGODB_URL);
+  // await mongoose.connect("mongodb://localhost:27017/JE-Advertisement-Web-App");
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log("DB connected Successfully!");
 };
 
